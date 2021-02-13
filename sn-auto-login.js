2cf12b5504aab3a0d7d253a822752bde6ad69eff
sparcc <sparco.209@gmail.com>
2021-02-13 11:25:04 +1100

    Initial working solution for windows

---
+++ sn-auto-login.js
@@ -0,0 +1,41 @@
+const { Builder, By, Key, until } = require("selenium-webdriver");
+
+const fs = require("fs");
+
+
+// Login details file name is "./login-details.json"
+  // Login details format:
+  /* {
+    "instance_hostname" : "https://devXXXXXX.service-now.com",
+    "username": "XXXXXXXXXXXX",
+    "password": "XXXXXXXXXXXXXX"
+  } */
+
+
+fs.readFile("./login-details.json", "utf8", (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  let loginDetails = JSON.parse(data);
+  runAutomation(loginDetails);
+});
+
+function runAutomation(loginDetails) {
+  (async function example() {
+    let driver = await new Builder().forBrowser("chrome").build();
+    try {
+      await driver.get(loginDetails.instance_hostname + "/login.do");
+      await driver.wait(until.titleIs("ServiceNow"), 10000);
+      await driver
+        .findElement(By.id("user_name"))
+        .sendKeys(loginDetails.username);
+      await driver
+        .findElement(By.id("user_password"))
+        .sendKeys(loginDetails.password);
+      await driver.findElement(By.id("sysverb_login")).click();
+    } finally {
+      await driver.quit();
+    }
+  })();
+}
