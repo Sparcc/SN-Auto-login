4cf7c374d0cc8da42621e6f2279ec4e09d936432
sparcc <sparco.209@gmail.com>
2021-02-13 20:48:29 +1100

    Now runs multiple user logins

--- sn-auto-login.js
+++ sn-auto-login.js
@@ -3,36 +3,43 @@ const { Builder, By, Key, until } = require("selenium-webdriver");
 const fs = require("fs");
 const { Options } = require("selenium-webdriver/chrome");
 
-
 // Login details file name is "./login-details.json"
-  // Login details format:
-  /* {
-    "instance_hostname" : "https://devXXXXXX.service-now.com",
-    "username": "XXXXXXXXXXXX",
-    "password": "XXXXXXXXXXXXXX"
-  } */
-
-
+// Login details format:
+/*  [
+      {
+        "instance_hostname" : "https://devXXXXXX.service-now.com",
+        "username": "XXXXXXXXXXXX",
+        "password": "XXXXXXXXXXXXXX"
+      },
+      {
+        "instance_hostname" : "https://devXXXXXX.service-now.com",
+        "username": "XXXXXXXXXXXX",
+        "password": "XXXXXXXXXXXXXX"
+      }
+    ]
+  */
 fs.readFile("./login-details.json", "utf8", (err, data) => {
   if (err) {
     console.error(err);
     return;
   }
-  let loginDetails = JSON.parse(data);
-  runAutomation(loginDetails);
+  let loginDetailsList = JSON.parse(data);
+  let options = new Options();
+  options.headless();
+  options.addArguments("--no-sandbox");
+  options.addArguments("--disable-dev-shm-usage");
+  (function runAutomationAsync() {
+    runAutomation(loginDetailsList,options);
+  })();
 });
 
-function runAutomation(loginDetails) {
-  (async function example() {
-    let options = new Options;
-    options.headless();
-    options.addArguments("--no-sandbox");
-    options.addArguments("--disable-dev-shm-usage");
-    let driver = await new Builder()
-    .forBrowser("chrome").setChromeOptions(options)
-    .build();
-
-    try {
+async function runAutomation(loginDetailsList, options) {
+  try {
+    loginDetailsList.forEach(async (loginDetails) => {
+      let driver = await new Builder()
+      .forBrowser("chrome")
+      .setChromeOptions(options)
+      .build();
       await driver.get(loginDetails.instance_hostname + "/login.do");
       await driver.wait(until.titleIs("ServiceNow"), 10000);
       await driver
@@ -42,8 +49,8 @@ function runAutomation(loginDetails) {
         .findElement(By.id("user_password"))
         .sendKeys(loginDetails.password);
       await driver.findElement(By.id("sysverb_login")).click();
-    } finally {
-      await driver.quit();
-    }
-  })();
+    });
+  } finally {
+    driver.quit();
+  }
 }
